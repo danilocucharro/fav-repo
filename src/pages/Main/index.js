eb45d0c39ae5b3b20f3121a1261ec78fb41a593b
danilocucharro <danicucharro@outlook.com>
2023-05-23 10:08:20 -0300

    foi adicionado a lista com os repositorios adicionados pelo usuario

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from 'react'
-import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa'
-import { Container, Form, SubmitButton } from './styles'
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa'
+import { Container, Form, SubmitButton, List, DeleteButton } from './styles'
 
 import api from '../../services/api'
 
@@ -37,6 +37,11 @@ export default function Main(){
         //console.log(newRepo)
     }
 
+    const handleDeleteRepo = useCallback((repo)=> {
+      const find = repositorios.filter(r => r.name !== repo);
+      setRepositorios(find);
+    }, [repositorios])
+
     return(
         <Container>
             <h1>
@@ -60,6 +65,22 @@ export default function Main(){
                     )}
                 </SubmitButton>
             </Form>
+
+            <List>
+              {repositorios.map(repo => (
+                <li key={repo.name}>
+                  <span>
+                    <DeleteButton onClick={()=>handleDeleteRepo(repo.name)}>
+                      <FaTrash size={14} />
+                    </DeleteButton>
+                    {repo.name}
+                  </span>
+                  <a href="">
+                    <FaBars size={20} />
+                  </a>
+                </li>
+              ))}
+            </List>
         </Container>
     );
 }
