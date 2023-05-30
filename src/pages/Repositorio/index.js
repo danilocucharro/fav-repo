4a93e873e44980d087d286c47b4a53101e1b8614
danilocucharro <danicucharro@outlook.com>
2023-05-30 10:25:01 -0300

    agora a pagina do repositório carrega as postagens de issues dos usuarios

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 import { useParams } from 'react-router-dom'
-import { Container, Owner, Loading, BackButton } from './styles'
+import { Container, Owner, Loading, BackButton, IssuesList, PageActions } from './styles'
 import { FaArrowLeft } from 'react-icons/fa'
 
 export default function Repositorio(){
@@ -49,12 +49,37 @@ export default function Repositorio(){
       </BackButton>
       <Owner>
         <img
-         src={repositorio.owner.avatar_url} 
-         alt={repositorio.owner.login} 
+         src={repositorio.owner.avatar_url}
+         alt={repositorio.owner.login}
         />
         <h1>{repositorio.name}</h1>
         <p>{repositorio.description}</p>
       </Owner>
+
+      <IssuesList>
+        {issues.map(issue =>(
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+            <div>
+              <strong>
+                <a href={issue.html_url} target="blank">{issue.title}</a>
+                {issue.labels.map(label =>(
+                  <span key={String(label.id)}>
+                    {label.name}
+                  </span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
+
+      <PageActions>
+        <button type="button" onClick={()=>{}}>Voltar</button>
+        <button type="button" onClick={()=>{}}>Próxima</button>
+      </PageActions>
     </Container>
   )
 }
